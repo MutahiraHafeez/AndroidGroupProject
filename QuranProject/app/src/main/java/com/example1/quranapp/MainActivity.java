363263ccde38dbb09c042ba1a10231eb09999ad3
Mutahira Hafeez <mutahira.2021@gmail.com>
2021-06-27 23:12:19 +0500

    Added Pdf

--- QuranProject/app/src/main/java/com/example1/quranapp/MainActivity.java
+++ QuranProject/app/src/main/java/com/example1/quranapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example1.quranapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView pdfListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pdfListView=(ListView)findViewById(R.id.Surah_Names);
+
+        String [] pdfFileNames={"Surah Kahf","Sural Mulk","Surah Ar Rehman","Surah Waqiah","Surah Yaseen","Surah Taubah"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>()
+
     }
 }
