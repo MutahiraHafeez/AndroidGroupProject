ba8b2946251735faff578b7eb63187c39a4dd0dc
Mutahira Hafeez <mutahira.2021@gmail.com>
2021-06-27 23:52:29 +0500

    Done with Data entry

--- QuranProject/app/src/main/java/com/example1/quranapp/MainActivity.java
+++ QuranProject/app/src/main/java/com/example1/quranapp/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example1.quranapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +27,29 @@ public class MainActivity extends AppCompatActivity {
         pdfListView=(ListView)findViewById(R.id.Surah_Names);
 
         String [] pdfFileNames={"Surah Kahf","Sural Mulk","Surah Ar Rehman","Surah Waqiah","Surah Yaseen","Surah Taubah"};
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>()
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,pdfFileNames)
+        {
 
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view=super.getView(position, convertView, parent);
+                TextView textView= (TextView) view.findViewById(android.R.id.text1);
+
+                return view ;
+            }
+        };
+
+        pdfListView.setAdapter(adapter);
+
+        pdfListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = pdfListView.getItemAtPosition(position).toString();
+                Intent intent = new Intent(getApplicationContext(),PdfOpener.class);
+                intent.putExtra("fileName",item);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ QuranProject/app/src/main/java/com/example1/quranapp/PdfOpener.java
@@ -0,0 +1,14 @@
+package com.example1.quranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PdfOpener extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_opener);
+    }
+}
